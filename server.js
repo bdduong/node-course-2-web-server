85ce2060f062745aeb4a1bdef7dc884f80fbda03
Bryan Duong <bryandduong@gmail.com>
2018-10-27 14:38:06 -0700

    ready for heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 9000;
 const app = express();
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
@@ -51,4 +52,4 @@ app.get('/bad', (req, res) => {
   })
 })
 
-app.listen(9000, () => console.log('Listening on port 9000!'))
+app.listen(port, () => console.log(`Listening on port ${port}!`))
