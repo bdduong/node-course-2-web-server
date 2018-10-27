00c02c5a1275e43574e3677364c56678771ec25e
Bryan Duong <bryandduong@gmail.com>
2018-10-27 15:01:50 -0700

    added project page

--- server.js
+++ server.js
@@ -42,9 +42,15 @@ app.get('/', (req, res) => {
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
-    pageTitle: 'About Page',
+    pageTitle: 'About Page'
   });
 });
+
+app.get('/project', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Project Page'
+  })
+})
 
 app.get('/bad', (req, res) => {
   res.send({
